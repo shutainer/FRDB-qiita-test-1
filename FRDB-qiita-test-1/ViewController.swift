6727e3532c5856a066cbf32755b7232c64590370
shutainer <shutainer@yahoo.co.jp>
2020-06-26 17:52:27 +0900

    Initial Commit

---
+++ FRDB-qiita-test-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FRDB-qiita-test-1
+//
+//  Created by 須藤英隼 on 2020/06/26.
+//  Copyright © 2020 Eishun Sudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
